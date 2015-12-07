4c2d883b12bba0f2623228dc5b7f30a3e89e1b32
Jason Gaare <jason.d.gaare@gmail.com>
2015-12-07 10:29:49 -0600

    Initial Commit. Let's go!

--- BracketBaker/ViewController.swift
+++ BracketBaker/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Made it!
+        print("Successful Load.")
     }
 
     override func didReceiveMemoryWarning() {
