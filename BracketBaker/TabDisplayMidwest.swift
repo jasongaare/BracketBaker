8faedb879e0f5619867da2a46571c7d052672314
Jason Gaare <jason.d.gaare@gmail.com>
2016-01-12 15:29:06 -0600

    --Drawing lines

--- BracketBaker/TabDisplayMidwest.swift
+++ BracketBaker/TabDisplayMidwest.swift
@@ -12,19 +12,61 @@ class TabDisplayMidwest: UIView {
 
     var team1 : UILabel
     var team2 : UILabel
+    var team3 : UILabel
+    var team4 : UILabel
+    var team5 : UILabel
+    var team6 : UILabel
+    var team7 : UILabel
+    var team8 : UILabel
     
     //This is how we draw everything
     override func drawRect(rect: CGRect)
     {
         let spacer : CGFloat = 5
         
+        // This makes some settings things
         let context = UIGraphicsGetCurrentContext()
         CGContextSetLineWidth(context, 2.0)
         let colorSpace = CGColorSpaceCreateDeviceRGB()
         let components: [CGFloat] = [0.0, 0.0, 1.0, 1.0]
         let color = CGColorCreate(colorSpace, components)
         CGContextSetStrokeColorWithColor(context, color)
+  /*
         
+        // Loop through pairings and draw lines for each matchup
+        // There are 4 matchups for now
+        var startTeam : Int
+        var teamA : UILabel
+        var teamB : UILabel
+        var teamALeft : CGPoint
+        var teamARight : CGPoint
+        var teamBLeft : CGPoint
+        var teamBRight : CGPoint
+        
+        for i in 0...3 {
+            startTeam = i*2
+            
+            // Get team labels from array
+            teamA = labelArray[startTeam]
+            teamB = labelArray[startTeam+1]
+            
+            // Get points for lines
+            teamALeft = CGPointMake(teamB.frame.origin.x, CGRectGetMaxY(teamA.frame) + spacer)
+            teamARight = CGPointMake(CGRectGetMaxX(teamA.frame), CGRectGetMaxY(teamA.frame) + spacer)
+            teamBRight = CGPointMake(CGRectGetMaxX(teamB.frame), CGRectGetMaxY(teamB.frame) + spacer)
+            teamBLeft = CGPointMake(teamB.frame.origin.x, CGRectGetMaxY(teamB.frame) + spacer)
+            
+            // Guide the lines
+            CGContextMoveToPoint(context, teamALeft.x, teamALeft.y)
+            CGContextAddLineToPoint(context, teamARight.x, teamARight.y)
+            CGContextAddLineToPoint(context, teamBRight.x, teamBRight.y)
+            CGContextAddLineToPoint(context, teamBLeft.x, teamBLeft.y)
+            
+            print("Loop \(i)")
+        }
+    */
+        
+        // Lines for Matchup #1
         let team1Left = CGPointMake(team1.frame.origin.x, CGRectGetMaxY(team1.frame) + spacer)
         let team1Right = CGPointMake(CGRectGetMaxX(team1.frame), CGRectGetMaxY(team1.frame) + spacer)
         let team2Right = CGPointMake(CGRectGetMaxX(team2.frame), CGRectGetMaxY(team2.frame) + spacer)
@@ -34,15 +76,62 @@ class TabDisplayMidwest: UIView {
         CGContextAddLineToPoint(context, team1Right.x, team1Right.y)
         CGContextAddLineToPoint(context, team2Right.x, team2Right.y)
         CGContextAddLineToPoint(context, team2Left.x, team2Left.y)
+        
+        // Lines for Matchup #2
+        
+        let team3Left = CGPointMake(team3.frame.origin.x, CGRectGetMaxY(team3.frame) + spacer)
+        let team3Right = CGPointMake(CGRectGetMaxX(team3.frame), CGRectGetMaxY(team3.frame) + spacer)
+        let team4Right = CGPointMake(CGRectGetMaxX(team4.frame), CGRectGetMaxY(team4.frame) + spacer)
+        let team4Left = CGPointMake(team4.frame.origin.x, CGRectGetMaxY(team4.frame) + spacer)
+
+        CGContextMoveToPoint(context, team3Left.x, team3Left.y)
+        CGContextAddLineToPoint(context, team3Right.x, team3Right.y)
+        CGContextAddLineToPoint(context, team4Right.x, team4Right.y)
+        CGContextAddLineToPoint(context, team4Left.x, team4Left.y)
+        
+        // Lines for Matchup #3
+        
+        let team5Left = CGPointMake(team5.frame.origin.x, CGRectGetMaxY(team5.frame) + spacer)
+        let team5Right = CGPointMake(CGRectGetMaxX(team5.frame), CGRectGetMaxY(team5.frame) + spacer)
+        let team6Right = CGPointMake(CGRectGetMaxX(team6.frame), CGRectGetMaxY(team6.frame) + spacer)
+        let team6Left = CGPointMake(team6.frame.origin.x, CGRectGetMaxY(team6.frame) + spacer)
+        
+        CGContextMoveToPoint(context, team5Left.x, team5Left.y)
+        CGContextAddLineToPoint(context, team5Right.x, team5Right.y)
+        CGContextAddLineToPoint(context, team6Right.x, team6Right.y)
+        CGContextAddLineToPoint(context, team6Left.x, team6Left.y)
+
+        // Lines for Matchup #4
+        
+        let team7Left = CGPointMake(team7.frame.origin.x, CGRectGetMaxY(team7.frame) + spacer)
+        let team7Right = CGPointMake(CGRectGetMaxX(team7.frame), CGRectGetMaxY(team7.frame) + spacer)
+        let team8Right = CGPointMake(CGRectGetMaxX(team8.frame), CGRectGetMaxY(team8.frame) + spacer)
+        let team8Left = CGPointMake(team8.frame.origin.x, CGRectGetMaxY(team8.frame) + spacer)
+        
+        CGContextMoveToPoint(context, team7Left.x, team7Left.y)
+        CGContextAddLineToPoint(context, team7Right.x, team7Right.y)
+        CGContextAddLineToPoint(context, team8Right.x, team8Right.y)
+        CGContextAddLineToPoint(context, team8Left.x, team8Left.y)
+        
+        // Draw those lines!
         CGContextStrokePath(context)
         
     }
     
+    // Initializer
     required init?(coder aDecoder: NSCoder) {
         
+        // Get these puppies cooking
         self.team1 = UILabel()
         self.team2 = UILabel()
+        self.team3 = UILabel()
+        self.team4 = UILabel()
+        self.team5 = UILabel()
+        self.team6 = UILabel()
+        self.team7 = UILabel()
+        self.team8 = UILabel()
         
+        // Let's go up and init too
         super.init(coder: aDecoder)
     }
 }
--- BracketBaker/TabDisplayMidwestController.swift
+++ BracketBaker/TabDisplayMidwestController.swift
@@ -20,10 +20,23 @@ class TabDisplayMidwestController: UIViewController {
         
         winner1.text = "\(midwestArray[0][2])"
         winner2.text = "\(midwestArray[1][2])"
+        winner3.text = "\(midwestArray[2][2])"
+        winner4.text = "\(midwestArray[3][2])"
+        winner5.text = "\(midwestArray[4][2])"
+        winner6.text = "\(midwestArray[5][2])"
+        winner7.text = "\(midwestArray[6][2])"
+        winner8.text = "\(midwestArray[7][2])"
 
         let canvas = super.view as! TabDisplayMidwest
         canvas.team1 = winner1
         canvas.team2 = winner2
+        canvas.team3 = winner3
+        canvas.team4 = winner4
+        canvas.team5 = winner5
+        canvas.team6 = winner6
+        canvas.team7 = winner7
+        canvas.team8 = winner8
+        
     }
     
 
@@ -31,6 +44,12 @@ class TabDisplayMidwestController: UIViewController {
     // MARK : Outlets (galore!)
     @IBOutlet weak var winner1: UILabel!
     @IBOutlet weak var winner2: UILabel!
+    @IBOutlet weak var winner3: UILabel!
+    @IBOutlet weak var winner4: UILabel!
+    @IBOutlet weak var winner5: UILabel!
+    @IBOutlet weak var winner6: UILabel!
+    @IBOutlet weak var winner7: UILabel!
+    @IBOutlet weak var winner8: UILabel!
 
     
     
